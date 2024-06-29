6e2830ec5012870b28073702e434063a9d62a5d4
ImJustNon <Im_Just_Non@hotmail.com>
2024-06-30 01:29:00 +0700

    Feat : Add boxDeepInfo

---
+++ controllers/boxDeepInfo_controller.js
@@ -0,0 +1,98 @@
+const jwt = require("jsonwebtoken");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const config = require("../config/config");
+const axios = require("axios");
+const { getJwtFromBearerHeader } = require("../utils/getJwtFromBearerHeader");
+const { verifyJwt } = require("../utils/verifyJwt");
+const { convertDateObjToISOString } = require("../utils/convertDateObjToISOString");
+const { isValidDate } = require("../utils/isValidDate");
+
+async function BoxDeepInfoController(req, res){
+    const { token } = req.cookies ?? {};
+    const { boxUUID } = req.body ?? {};
+    
+    if(!token){
+        return res.json({
+            status: "FAIL",
+            message: "Require token",
+            error: {},
+        });  
+    }
+
+    if(!boxUUID){
+        return res.json({
+            status: "FAIL",
+            message: "Require Box UUID",
+            error: {},
+        }); 
+    }
+
+    // parseJwtToRealData
+    const getTokenFronJwt = await verifyJwt(token);
+    if(!getTokenFronJwt){
+        return res.json({
+            status: "FAIL",
+            message: "Currupted User Token",
+            error: {},
+        });
+    }
+
+    // verify user token
+    try {
+        // check user uuid
+        const getUserData = await prisma.user.findUnique({
+            where: {
+                user_uuid: getTokenFronJwt.uuid
+            },
+            select: {
+                user_uuid: true,
+            }
+        });
+
+        if(!getUserData){
+            return res.json({
+                status: "FAIL",
+                message: "User not found",
+                error: {},
+            });
+        }
+
+        const findBoxInfo = await prisma.box.findUnique({
+            where: {
+                box_uuid: boxUUID,
+            },
+            select: {
+                id: true,
+                box_key: true,
+                box_slot_count: true
+            }
+        });
+
+        if(!findBoxInfo){
+            return res.json({
+                status: "FAIL",
+                message: "Box not found",
+                error: {}
+            });
+        }
+
+        return res.json({
+            status: "OK",
+            message: "Box data found",
+            error: null,
+            data: findBoxInfo
+        });
+    }
+    catch(e) {
+        return res.json({
+            status: "FAIL",
+            message: "Sothing went wrong",
+            error: e
+        });
+    }
+}
+
+module.exports = {
+    BoxDeepInfoController
+}
--- routes/box_route.js
+++ routes/box_route.js
@@ -5,6 +5,7 @@ const { BoxListController } = require("../controllers/boxList_controller");
 const { BoxInfoController } = require("../controllers/BoxInfo_controller");
 const { BoxRegisterController } = require("../controllers/boxRegister_controller");
 const { BoxTodayAlertController } = require("../controllers/boxTodayAlert_controller");
+const { BoxDeepInfoController } = require("../controllers/boxDeepInfo_controller");
 const urlEncoded = bodyparser.urlencoded({
     limit: "50mb",
     extended: false,
@@ -15,6 +16,7 @@ router.post("/v1/box/list", urlEncoded, BoxListController);
 router.post("/v1/box/info", urlEncoded, BoxInfoController);
 router.post("/v1/box/register", urlEncoded, BoxRegisterController);
 router.post("/v1/box/todayalert", urlEncoded, BoxTodayAlertController);
+router.post("/v1/box/deepinfo", urlEncoded, BoxDeepInfoController);
 
 
 
